2b870546906dac8c4157d405ed25cd044f309066
Ayush <ap4769883@gmail.com>
2026-09-13 11:22:01 +0000

    Temporarily disabled database connection to avoid errors during initial setup and test runs.

--- crudSpringBoot/src/main/java/in/ayush/crudSpringBoot/CrudSpringBootApplication.java
+++ crudSpringBoot/src/main/java/in/ayush/crudSpringBoot/CrudSpringBootApplication.java
@@ -3,7 +3,7 @@ package in.ayush.crudSpringBoot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CrudSpringBootApplication {
 
 	public static void main(String[] args) {
---
+++ crudSpringBoot/src/main/java/in/ayush/crudSpringBoot/StudentService.java
@@ -0,0 +1,8 @@
+package in.ayush.crudSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentService {
+    // Service methods for student operations can be added here
+}
