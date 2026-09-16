84c5c9e9cbe092c17d6f633aaf91c575ad1f6b53
Ayush <ap4769883@gmail.com>
2026-09-16 07:50:02 +0000

    working on the controller class , mapped the controller with the end point and created createStudent method

--- crudSpringBoot/src/main/java/in/ayush/crudSpringBoot/controller/StudentController.java
+++ crudSpringBoot/src/main/java/in/ayush/crudSpringBoot/controller/StudentController.java
@@ -1,7 +1,24 @@
 package in.ayush.crudSpringBoot.controller;
 
-
+@RestController
+@RequestMapping("/api/students")
 public class StudentController{
    //1.Listen end-points(/api/students POST, GET, PUT, DELETE)(Student Controller)
+   //create Student (POST)
+   @PostMapping
+   public void createStudent(@RequestBody Student student) {
+       //2. Call Service Layer
+   }
+
+   //read Student (GET)
+
+   public List<Student> getAllStudents(){
+
+   }
+
+   //update Student (PUT)
+
+   //delete Student (DELETE)
+
  
 }
